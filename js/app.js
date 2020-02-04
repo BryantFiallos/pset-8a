0597a71914f14e2eb2bbda1d113d1e049f247a06
Bryant Fiallos <bryant.fiallos@gmail.com>
2020-02-04 10:29:50 -0500

    app

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ const message = document.querySelector("h2");
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init() {
   board = [
@@ -34,19 +35,39 @@ function init() {
 
 function render() {
   board.forEach(function(mark, index) {
-    squares[index].textContent = mark;    // writes an X or an O on board
+    squares[index].textContent = mark;
   });
 
-  message.textContent = `Turn: ${turn}`;
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
 }
 
 function takeTurn(e) {
-  let index = squares.findIndex(function(square) {
-    return square === e.target;
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    board[index] = turn;
+    turn = turn === "X" ? "O" : "X";
+    win = getWinner();
+
+    render();
+  }
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
   });
 
-  board[index] = turn;
-  turn = turn === "X" ? "O" : "X";
-
-  render();
+  return winner ? winner : board.includes("") ? null : "T";
 }
