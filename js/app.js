b34a123450623c46ab9b300326ee0921a2b625a6
Bryant Fiallos <bryant.fiallos@gmail.com>
2020-02-04 10:24:26 -0500

    committ

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
-
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;
 let turn;
+let win;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
@@ -16,6 +27,7 @@ function init() {
     "", "", ""
   ];
   turn = "X";
+  win = null;
 
   render();
 }
@@ -24,6 +36,8 @@ function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;    // writes an X or an O on board
   });
+
+  message.textContent = `Turn: ${turn}`;
 }
 
 function takeTurn(e) {
